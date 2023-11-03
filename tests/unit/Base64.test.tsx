2a5a773a4c5d7cb602774c45bb8f9d7868c96960
Daniel M. Halverson <daniel.m.halverson@gmail.com>
2023-11-03 12:25:52 +0000

    adding more tests

--- tests/unit/Base64.test.tsx
+++ tests/unit/Base64.test.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Base64 from "../../app/components/Base64";
 
-test("Base64 component", () => {
-    const unit = <Base64 />;
-    const component = render(unit);
-    const tree = component.container;
-    expect(tree.toString()).toMatchSnapshot();
-});
+describe("Base64", () => {
+    it("Will produce a snapshot of the Base64 component", () => {
+        const component = render(<Base64 />);
+        expect(component.container.toString()).toMatchSnapshot();
+    });
+
+    it("Will test the Base64 component encode functionality", async() => {
+        render(<Base64 />);
+        await userEvent.type(document.getElementById("encodeInput"), 'jpmc1234');
+        await userEvent.click(document.getElementById("encode"));
+        expect(document.getElementById("encodeOutput").value).toEqual('anBtYzEyMzQ=');
+    });
+
+    it("Will test the Base64 component decode functionality", async() => {
+        render(<Base64 />);
+        await userEvent.type(document.getElementById("decodeInput"), 'anBtYzEyMzQ=');
+        await userEvent.click(document.getElementById("decode"));
+        expect(document.getElementById("decodeOutput").value).toEqual('jpmc1234');
+    });
+})
+
